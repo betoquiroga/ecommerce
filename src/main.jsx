2ee0dbbbe717d5c9668dc0ee031cd9ef4a15e333
Beto Jr <beto.m12@gmail.com>
2022-11-07 12:00:41 -0400

    Se inicia los estilos de Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from "./router/Router"
+import "./styles/index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
